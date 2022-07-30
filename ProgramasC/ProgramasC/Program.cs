efe4efbf081cd4149e966c1f97aaaef0f4925df0
LuizFelipe13 <luiz_felipesbc@hotmail.com>
2022-07-30 19:54:14 -0300

    Soma e multiplicação
    Criação de programa de consola para soma e multiplicação

---
+++ ProgramasC/ProgramasC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProgramasC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, soma, multiplicacao;
+            Console.WriteLine("Informe o primeiro número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+            multiplicacao = soma * num1;
+
+            Console.WriteLine("O resultado da multiplicação é: " + multiplicacao);
+        }
+    }
+}
